0ea05890fe0b59b83c826af9b2cb0ce8fc2c1a84
sampado <sampado@gmail.com>
2016-02-03 16:30:35 -0300

    Spring Data with MongoDB

---
+++ spring-data-mongodb/src/main/java/com/sampa/spring_data_mongodb/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.sampa.spring_data_mongodb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.sampa.spring_data_mongodb.model.Product;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
